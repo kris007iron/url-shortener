6df456fffcdcf63a300b90e59f26900410a4e109
Krzysztof Mrugała <66689589+kris007iron@users.noreply.github.com>
2024-09-03 18:15:52 +0200

    chore: Remove unused database tables and frontend files

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,20 @@
 use nanoid;
+use rocket::fs::NamedFile;
 use rocket::{get, http::Status, post, response::Redirect, routes, State};
 use sqlx::{Error, PgPool};
+use std::path::Path;
 use url::Url;
 
 #[get("/")]
-fn index() -> &'static str {
-    "Hello, world!"
+async fn index() -> Option<NamedFile> {
+    NamedFile::open(Path::new("frontend/index.html")).await.ok()
 }
 
+// Serve the 1712.html file when accessing "/1712"
+#[get("/1712")]
+async fn special_page() -> Option<NamedFile> {
+    NamedFile::open(Path::new("frontend/1712.html")).await.ok()
+}
 #[get("/<id>")]
 async fn redirect(id: String, pool: &State<PgPool>) -> Result<Redirect, Status> {
     let url: (String,) = sqlx::query_as("SELECT url FROM urls WHERE id = $1")
@@ -37,7 +44,7 @@ async fn shorten(url: String, pool: &State<PgPool>) -> Result<String, Status> {
 #[shuttle_runtime::main]
 async fn main(#[shuttle_shared_db::Postgres] _pool: PgPool) -> shuttle_rocket::ShuttleRocket {
     let rocket = rocket::build()
-        .mount("/", routes![index])
+        .mount("/", routes![index, special_page])
         .mount("/", routes![redirect, shorten])
         .manage(_pool);
     Ok(rocket.into())
